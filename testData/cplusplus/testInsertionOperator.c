c8b5052c97db7f90b55121256ce21aca5b716270
Siddhesh Vethe <sivethe@microsoft.com>
2025-08-15 22:57:25 -0700

    Enhance test cases for string literals and output validation in C++ tests

--- testData/cplusplus/testInsertionOperator.c
+++ testData/cplusplus/testInsertionOperator.c
@@ -17,3 +17,16 @@ return {Err100,
                       << "Function was called with arguments: " 
                       << this->arguments.size()};
 
+// Simple out parameters
+std::cout << "Testing std::cout =>Expected A1 instead of ";
+std::cout << "Testing std::cout =>Expected A2 instead of " << documentType;
+std::cout << "Testing std::cout =>Expected A3 instead of " << documentType << std::endl;
+std::cerr << "Testing std::cerr =>Expected B1 instead of ";
+std::cerr << "Testing std::cerr =>Expected B2 instead of " << documentType;
+std::cerr << "Testing std::cerr =>Expected B3 instead of " << documentType << std::endl;
+
+// Test adjacent string literals
+std::cout << "Testing adjacent-string-literals =>Expected C1 instead of " << documentType
+          << ". Please specify C2"
+          << " or any other variable in the C* family"
+          << " for this scenario to work." << std::endl;
--- testData/cplusplus/testStringLiteralsInFunctions.c
+++ testData/cplusplus/testStringLiteralsInFunctions.c
@@ -1,1 +1,4 @@
 return Outcome(Err100, "User is not authorized to perform this operation");
+Assert(Err100, 
+       "Unauthenticated user is trying to perform an operation",
+       authState != "Authorized");
